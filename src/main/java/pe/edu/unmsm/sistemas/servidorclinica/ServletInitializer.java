cba6f3f2e66509ef26661ec068a532668aca9f6a
Angelo Raphael Ravello Aguado <aravello@blacksquat.com>
2018-11-19 21:15:57 -0500

    Guardando cambios

--- src/main/java/pe/edu/unmsm/sistemas/servidorclinica/ServletInitializer.java
+++ src/main/java/pe/edu/unmsm/sistemas/servidorclinica/ServletInitializer.java
@@ -1,13 +1,13 @@
-package pe.edu.unmsm.sistemas.servidorclinica;
+//package pe.edu.unmsm.sistemas.servidorclinica;
+//
+//import org.springframework.boot.builder.SpringApplicationBuilder;
+//import org.springframework.boot.web.support.SpringBootServletInitializer;
 
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
-
-public class ServletInitializer extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(Main.class);
-    }
-
-}
+//public class ServletInitializer extends SpringBootServletInitializer {
+//
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//        return application.sources(Main.class);
+//    }
+//
+//}
